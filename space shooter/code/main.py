6a668322ffde34b2551930d016eb8f270e4b5c93
Nicolas <nicolasorobia@gmail.com>
2026-05-26 11:46:54 -0700

    learned how to upload images with join() and used list comprehension to place 20 stars randomly in display surface

--- space shooter/code/main.py
+++ space shooter/code/main.py
@@ -1,16 +1,23 @@
 import pygame
+from os.path import join
+from random import randint
 
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-pygame.display.set_caption("Space Shooter")
-pygame.display.set_icon(pygame.image.load('images/player.png'))
+pygame.display.set_caption("Space Shooter") # Configure title of game window
+pygame.display.set_icon(pygame.image.load('images\player.png')) # Configure icon image next to title
 running = True
 
-# surface
-surf = pygame.Surface((100,100))
-surf.fill('orange')
+# plain surface
+# surf = pygame.Surface((100,100))
+# surf.fill('orange')
+
+# importing an image
+player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
+star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
+star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT) ) for _ in range(20)]
 
 while running:
     # event loop
@@ -18,9 +25,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
     display_surface.fill('darkgray')
-    display_surface.blit(surf, (640, 360))
+    display_surface.blit(player_surf, ((1280/2) - (player_surf.get_width()/2), 310))
+
+    # place 20 stars surfaces randomly
+    for pos in star_positions:
+        display_surface.blit(star_surf, pos)
+
     pygame.display.update()
 
 pygame.quit()
